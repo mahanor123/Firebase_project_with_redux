d736912624009dfe264f1b25f0386f46828c352e
Your Name <you@example.com>
2020-01-15 16:04:30 +0530

    create CreateProject compnent

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from './component/layout/Navbar'
 import Dashboard from './component/dashboard/Dashboard';
 import ProjectDetails from './component/projects/ProjectDetails'
+import SingIn from './component/auth/SingIn';
+import SingUp from './component/auth/SingUp'
+import CreateProject from './component/projects/CreateProject'
 
-class App extends   Component {
+class App extends Component {
   render(){
   return (
     <BrowserRouter>
@@ -13,6 +16,9 @@ class App extends   Component {
         <Switch>
           <Route  exact path='/'component={Dashboard}/>
           <Route path='/project/:id'component={ProjectDetails}/>
+          <Route path='/singin'component={SingIn}/>
+          <Route path='/singUp'component={SingUp}/>
+          <Route path='/create'component={CreateProject}/>
         </Switch>
       </div>
     </BrowserRouter>
